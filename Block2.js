5f09c42ad28d5f19885be9a2f49c2e31e5585244
kruttika16 <84858573+kruttika16@users.noreply.github.com>
2021-06-03 19:47:59 +0530

    Add files via upload

---
+++ Block2.js
@@ -0,0 +1,26 @@
+class Block2{
+    constructor(x,y,width,height){
+        var options = {
+            'restitution' :0.4,
+            'friction' :1
+        }
+        this.body = Bodies.rectangle(x,y,width,height,options);
+        this.width = width;
+        this.height = height;
+
+        World.add(world, this.body);
+    }
+    display(){
+        Push ();
+        //translate(this.body.position.x,this.body.position.y);
+        var pos = this.body.position;
+        rectMode(CENTRE);
+        fill (rgb(49, 29, 161));
+        rect(pos.x, pos.y, this.width, this.height);
+
+        pop();
+
+
+    
+    }
+}
